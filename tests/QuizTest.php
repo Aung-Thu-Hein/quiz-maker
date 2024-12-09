1e938bab69b5e9ccd97d201f4766362d4b4e972e
[Aung-Thu-Hein] <aungthuhein225k@gmail.com>
2024-12-09 16:06:47 +0700

    feat: make more flexible quiz creation - create quiz, that mush have least one question - use same score for all questions or use different score for each question
    refactor: add getter, setter method for protected properties

--- tests/QuizTest.php
+++ tests/QuizTest.php
@@ -11,28 +11,38 @@ use PHPUnit\Framework\Attributes\Test;
 
 class QuizTest extends TestCase
 {
+
+    #[Test]
+    public function it_creates_a_quiz_with_one_question_at_least()
+    {
+        $quiz = new Quiz("Math Quiz", new Question("What is the smallest positive prime number?", "2", 2));
+
+        $this->assertCount(1, $quiz->getQuestions());
+    }
+
     #[Test]
     public function it_consists_of_questions()
     {
-        $quiz = new Quiz();
+        $quiz = new Quiz("Random Quiz", new Question("What is 4-2?", "2", 2));
 
-        $quiz->addQuestion(new Question("What is 2 + 2?", 4, 100));
+        $quiz->addQuestion(new Question("What is the largest country in the world?", "Russia", 5));
+        $quiz->addQuestion(new Question("In which year, the Covid19 happened?", "2020", 10));
 
-        $this->assertCount(1, $quiz->getQuestions());
+        $this->assertCount(3, $quiz->getQuestions());
     }
 
     #[Test]
     #[DataProvider('scoreProvider')]
     public function it_calculates_user_score_and_total_score(array $questions, array $answers, int $expectedUserScore, int $expectedTotalScore)
     {
-        $quiz = new Quiz();
+        $quiz = new Quiz("Random Quiz", new Question("What is the smallest positive number?", "0", 5));
 
-        for($i=0, $length = count($questions); $i<$length; $i++){
+        for ($i = 0, $length = count($questions); $i < $length; $i++) {
             $quiz->addQuestion($questions[$i]);
         }
 
-        foreach($answers as $index => $answer){
-            $quiz->getQuestions()[$index]->answer($answer);
+        foreach ($answers as $index => $answer) {
+            $quiz->getQuestions()[$index]->setAnswer($answer);
         }
 
         $userScore = $quiz->calculateUserScore();
@@ -48,37 +58,51 @@ class QuizTest extends TestCase
             //Case for all correct
             [
                 'questions' => [
-                    new Question("What is 2+2?", 4, 5),
-                    new Question("What is 4+4?", 8, 5),
-                    new Question("What is 3+3?", 6, 5),
+                    new Question("What is 2+2?", "4", 10),
+                    new Question("What symbol is used to declare the variable in PHP?", "$", 20),
+                    new Question("What is the first day of a year?", "January 1st", 30),
                 ],
-                'answers' => [4, 8, 6],
-                'expectedUserScore' => 15,
-                'expectedTotalScore' => 15
+                'answers' => ["0", "4", "$", "January 1st"],
+                'expectedUserScore' => 65,
+                'expectedTotalScore' => 65
             ],
 
             //Case for half correct
             [
                 'questions' => [
-                    new Question("What is 2+2?", 4, 5),
-                    new Question("What is 4+4?", 8, 5),
+                    new Question("What is 2+2?", "4", 1),
+                    new Question("What symbol is used to declare the variable in PHP?", "$", 1),
+                    new Question("What is the first day of a year?", "January 1st", 1),
                 ],
-                'answers' => [4, 1],
-                'expectedUserScore' => 5,
-                'expectedTotalScore' => 10
+                'answers' => ["1", "4", "var", "January 1st"],
+                'expectedUserScore' => 2,
+                'expectedTotalScore' => 8
             ],
 
             //Case for incorrect, score 0
             [
                 'questions' => [
-                    new Question("What is 2+2?", 4, 5),
-                    new Question("What is 4+4?", 8, 5),
-                    new Question("What is 3+3?", 6, 5),
+                    new Question("What is 2+2?", "4", 20),
+                    new Question("What symbol is used to declare the variable in PHP?", "$", 30),
+                    new Question("What is the first day of a year?", "January 1st", 40),
                 ],
-                'answers' => [6, 4, 8],
+                'answers' => ["9", "40", "var", "June 1st"],
                 'expectedUserScore' => 0,
-                'expectedTotalScore' => 15
+                'expectedTotalScore' => 95
             ]
         ];
+    }
+
+    #[Test]
+    public function it_uses_same_score_for_all_questions()
+    {
+        $quiz = new Quiz("Same Score Quiz", new Question("Question-1", "Answer-1", 2), true);
+
+        $quiz->addQuestion(new Question("Question-2", "Answer-2"));
+        $quiz->addQuestion(new Question("Questioin-3", "Answer-3"));
+
+        $totalScore = $quiz->calculateTotalScore();
+
+        $this->assertEquals(6, $totalScore);
     }
 }
