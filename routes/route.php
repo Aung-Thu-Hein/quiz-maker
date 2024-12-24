3410738844ac809adf161e52f6f6ab9e7907a01a
[Aung-Thu-Hein] <[aungthuhein225k@gmail.com]>
2024-12-25 00:53:32 +0700

    feat: add db config
    - add db instance into the container
    - load env

--- routes/route.php
+++ routes/route.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Quizzes\Controllers\QuizController;
+use Core\Container;
+use Core\DB;
 
 $router->get('/', function() {
-    echo "Default route";
+    $db = (new Container)->get(DB::class);
+
+    $query = 'SELECT * FROM products';
+    foreach($db->query($query) as $product) {
+        echo '<pre>';
+        var_dump($product);
+        echo '</pre>';
+    }
 });
 
 $router->post('/quiz', [QuizController::class, 'store']);
