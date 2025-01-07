92165f89dd931c6165d24f23bb4344edf31ed279
[Aung-Thu-Hein] <aungthuhein225k@gmail.com>
2025-01-07 22:43:34 +0700

    feat: add output formatting helper functions, dd and dump

--- core/functions.php
+++ core/functions.php
@@ -1,7 +1,6 @@
 <?php
 
 if(!function_exists('config')) {
-
     function config(string $env): mixed
     {
         $configs = require BASE_PATH . 'core/config.php';
@@ -12,5 +11,24 @@ if(!function_exists('config')) {
         
         return $configs[$env];
     }
+}
 
+if(!function_exists('dd')) {
+    function dd(mixed $var): void
+    {
+        echo '<pre>';
+        var_dump($var);
+        echo '</pre>';
+
+        die();
+    }
+}
+
+if(!function_exists('dump')) {
+    function dump(mixed $var): void
+    {
+        echo '<pre>';
+        var_dump($var);
+        echo '</pre>';
+    }   
 }
