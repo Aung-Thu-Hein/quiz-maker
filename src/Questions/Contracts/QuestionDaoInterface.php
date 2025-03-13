271bbc8d3b89bb86a564ec99641471da84eb5607
[Aung-Thu-Hein] <aungthuhein225k@gmail.com>
2025-03-13 16:23:02 +0700

    feat: add questions DAO layer - implement question CRUD operation in DAO layer

--- src/Questions/Contracts/QuestionDaoInterface.php
+++ src/Questions/Contracts/QuestionDaoInterface.php
@@ -6,5 +6,17 @@ use Core\DB;
 
 interface QuestionDaoInterface
 {
+    public function all(int $quiz_id): array;
+
+    public function show(int $id): array;
+
     public function create(array $data): DB;
+
+    public function createMultiple(int $quiz_id, array $data): DB;
+
+    public function update(int $id, array $data): DB;
+
+    public function patch(int $id, array $data): DB;
+
+    public function delete(int $id): bool;
 }
--- src/Questions/DAOs/QuestionDao.php
+++ src/Questions/DAOs/QuestionDao.php
@@ -13,9 +13,70 @@ class QuestionDao implements QuestionDaoInterface
         //
     }
 
+    public function all(int $quiz_id): array
+    {
+        $query = 'SELECT * FROM questions WHERE quiz_id = :quiz_id';
+        return $this->db->run($query, ['quiz_id' => $quiz_id])->all();
+    }
+
+    public function show(int $id): array
+    {
+        $query = 'SELECT * FROM questions WHERE id = :id';
+        return $this->db->run($query, ['id' => $id])->find();
+    }
+
     public function create(array $data): DB
     {
         $query = 'INSERT INTO questions (quiz_id, body, options, solution, score) VALUES (:quiz_id, :body, :options, :solution, :score)';
         return $this->db->run($query, $data);
+    }
+
+    public function createMultiple(int $quiz_id, array $data): DB
+    {
+        $query = 'INSERT INTO questions (quiz_id, body, options, solution, score) VALUES ';
+
+        $placeholders = [];
+        $values = [];
+
+        foreach($data as $index => $question){
+            $placeholders[] = "(?, ?, ?, ?, ?)";
+
+            //store in sequential order
+            $values[] = $quiz_id;
+            $values[] = $question['body'];
+            $values[] = json_encode($question['options']);
+            $values[] = json_encode($question['solution']);
+            $values[] = $question['score'];
+        }
+
+        $query .= implode(', ', $placeholders);
+        return $this->db->run($query, $values);
+    }
+
+    public function update(int $id, array $data): DB
+    {
+        $query = 'UPDATE questions SET quiz_id = :quiz_id, body = :body, options = :options, solution = :solution, score = :score WHERE id = :id';
+        $data['id'] = $id;
+        return $this->db->run($query, $data);
+    }
+
+    public function patch(int $id, array $data): DB
+    {
+        $fields = [];
+        foreach($data as $key => $value) {
+            $fields[] = "$key = :$key";
+        }
+
+        $query = 'UPDATE questions SET ' . implode(', ', $fields) . ' WHERE id = :id';
+        $data['id'] = $id;
+        return $this->db->run($query, $data);
+    }
+
+    public function delete(int $id): bool
+    {
+        $query = 'DELETE FROM questions WHERE id = :id';
+        $result = $this->db->delete($query, ['id' => $id]);
+
+        return $result > 0;
     }
 }
