2d89ae421c9164ec79d13fcf297a096e05b14999
[Aung-Thu-Hein] <aungthuhein225k@gmail.com>
2025-03-24 14:40:01 +0700

    feat: add getting label in question type enum

--- src/Enums/QuestionType.php
+++ src/Enums/QuestionType.php
@@ -6,4 +6,13 @@ enum QuestionType: int
 {
     case SINGLE_CHOICE = 1;
     case MULTIPLE_CHOICE = 2;
+
+    public static function getLabel(int $value): string
+    {
+        return match($value) {
+            self::SINGLE_CHOICE->value => 'Single Choice',
+            self::MULTIPLE_CHOICE->value => 'Multiple Choice',
+            default => throw new \InvalidArgumentException("Invalid question type value...")
+        };
+    }
 }
