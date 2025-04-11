2bcda340b8c521118c52d34ec2ddd7494e0983aa
Aung-Thu-Hein <aungthuhein225k@gmail.com>
2025-04-11 15:59:00 +0700

    fix: modify routes - remove patch route for quiz and question - change as 'store' instead of storeQuestions in route.php

--- routes/route.php
+++ routes/route.php
@@ -12,14 +12,11 @@ $router->get('/quizzes', [QuizController::class, 'index']);
 $router->get('/quizzes/{id}', [QuizController::class, 'show']);
 $router->post('/quizzes', [QuizController::class, 'store']);
 $router->put('/quizzes/{id}', [QuizController::class, 'update']);
-$router->patch('/quizzes/{id}', [QuizController::class, 'patch']);
 $router->delete('/quizzes/{id}', [QuizController::class, 'delete']);
 
-$router->get('/quizzes/{quiz_id}/questions', [QuestionController::class, 'index']);
 $router->get('/quizzes/{quiz_id}/questions/{id}', [QuestionController::class, 'show']);
-$router->post('/quizzes/{quiz_id}/questions', [QuestionController::class, 'storeQuestions']);
+$router->post('/quizzes/{quiz_id}/questions', [QuestionController::class, 'store']);
 $router->put('/quizzes/{quiz_id}/questions/{id}', [QuestionController::class, 'update']);
-$router->patch('/quizzes/{quiz_id}/questions/{id}', [QuestionController::class, 'patch']);
 $router->delete('/quizzes/{quiz_id}/questions/{id}', [QuestionController::class, 'delete']);
 
 $router->post('/users/register', [UserController::class, 'store']);
